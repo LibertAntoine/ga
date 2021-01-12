a5cd0a3c6fc91cc281aecdbf4131e6da5e55fddf
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-01-12 16:46:15 +0100

    [Setup] add Vue.js setup with Ganja canvas

---
+++ src/components/Game.vue
@@ -0,0 +1,34 @@
+<template>
+  <div id="game">
+      <div id="graph"></div>
+  </div>
+</template>
+
+<script>
+import Algebra from "ganja.js"
+
+export default {
+  name: 'Game',
+  mounted() {
+        let algebra = Algebra(2,0,1)
+
+        let calc = algebra.inline(() => {
+
+            //var point = (x,y)=>1e12-x*1e02+y*1e01;
+
+            return [
+                0x0000FF, 1e12, 1e1, 1e2,
+                0x000000, 1e12-1e02-1e01, "point",
+                0xff0000, [1e12+1e01,1e12+1e01+1e02], "segment",
+                0x007700, [1e12-1e01,1e12-1e01+1e02,1e12], 0x000000, "polygon"
+            ]
+        })
+        let graph = algebra.graph(calc())
+        console.log(graph)
+        document.getElementById("graph").appendChild(graph)
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/tools/Tool.js
@@ -0,0 +1,4 @@
+
+export default class Tool {
+    point = (x=0,y=0)=>!(1e0 + x*1e1 + y*1e2);
+}
