37882adeef8edf86b6028d3309e585b6365fc631
AntoineLibert <duncan-arrakis@hotmail.fr>
2021-02-09 10:02:22 +0100

    [GUI] Implement game gui

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,54 @@
 <template>
   <div id="app">
-    <h1>Ganja Game</h1>
+    <Title/>
     <Game/>
   </div>
 </template>
 
 <script>
 import Game from "@/components/Game"
+import Title from "@/components/Title"
 
 export default {
   name: 'App',
   components: {
-    Game
+    Game,
+    Title
   }
 }
 </script>
 
 <style>
+
+@font-face {
+  font-family: neon;
+  src: url("/fonts/denverFont.ttf");
+}
+
+body {
+  background-image: url("./assets/back6.jpg");
+  background-size: cover;
+  background-position: bottom;
+}
+
+body, html {
+  height :100vh;
+  width: 100%;
+  overflow: hidden;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  height: 100%;
+  padding-bottom : 30px;
+  padding-top  : 30px;
 }
+
 </style>
